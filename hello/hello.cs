7e8d8127af44bd1d2222e7ee8e7ebed371cc37a3
Bogi <nagybogi1023@gmail.com>
2018-12-26 17:38:52 +0100

    hello.sln comments

---
+++ hello/hello.cs
@@ -0,0 +1,7 @@
+public class Program
+{
+	static void Main()
+	{
+		System.Console.WriteLine(DateTime.Now.DayOfWeek);
+	}
+}
--- hello/hello/Program.cs
+++ hello/hello/Program.cs
@@ -1,13 +1,14 @@
-﻿//csinál egy name space-t
-//ettől a programunk neve igazából nem is 'Program', hanem
-//hello.Program
-using System;
+﻿using System;
 
-namespace hello
+namespace Hello
 {
     class Program
+    //mivel ez a Program nevű class a Hello namespace-ben van, ezért ez igazából Hello.Program
     {
         static void Main(string[] args)
+        // az 'args' miatt be tud veni külön dolgokat, mint ahogy lejjebb van a ReadLine-al
+        // ezek a beadott argumentumok oda lesznek adva a main metódusnak
+        // --> tehát a main method kaphat args nevű tömbbe értékeket (lejjebb ez a név és az óra, amit megadunk)
         {
             Console.WriteLine("your name?");
             string name = Console.ReadLine();
